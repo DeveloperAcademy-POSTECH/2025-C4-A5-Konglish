ceb405faa1b2fd4e6604fc6a101813e50a9be799
0tak2 <0tak2.code@gmail.com>
2025-07-22 19:33:44 +0900

    ✨ [Feat] ARContainer 추가 기능에 대응

--- Konglish/ARCore/Sources/UIComponents/ARContainer.swift
+++ Konglish/ARCore/Sources/UIComponents/ARContainer.swift
@@ -17,13 +17,17 @@ import SwiftUI
  import ARCore
 
  public struct ContentView: View {
-     @State var currentDetectedPlanes: Int = 0
-     @State var cardScatterTrigger = false
+     @State var arError: Error? // ARCore 발생 에러
+     @State var currentDetectedPlanes: Int = 0 // 현재 인식한 평면 수
+     @State var triggerScanStart = false // 평면 스캔 시작
+     @State var triggerPlaceCards = false // 카드 배치 시작
+     @State var gamePhase: GamePhase = .initialized // 현재 게임 단계
      
      let gameCards: [GameCard] = [
          .init(id: UUID(uuidString: "00000000-0000-0000-0000-000000000000")!, imageName: "apple", wordKor: "사과", wordEng: "apple"),
-          /* 생략 */
+         // More game cards
      ]
+     
      
      public init() {}
      
@@ -32,14 +36,32 @@ import SwiftUI
              ARContainer(
                  gameSettings: GameSettings(
                      gameCards: gameCards,
-                     minimumSizeOfPlane: 4
+                     minimumSizeOfPlane: 0.5
                  ),
-                 currentDetectedPlanes: $currentDetectedPlanes
+                 gamePhase: $gamePhase,
+                 arError: $arError,
+                 currentDetectedPlanes: $currentDetectedPlanes,
+                 triggerScanStart: $triggerScanStart,
+                 triggerPlaceCards: $triggerPlaceCards
              )
              .ignoresSafeArea()
              
              VStack {
+                 Text("gamePhase: \(gamePhase)")
+                 
                  Text("currentDetectedPlanes: \(currentDetectedPlanes)")
+                 
+                 Button("스캔 시작") {
+                     triggerScanStart = true
+                 }
+                 
+                 Button("카드 배치") {
+                     triggerPlaceCards = true
+                 }
+                 
+                 if let arError = arError {
+                     Text("에러: \(arError.localizedDescription)")
+                 }
                  
                  Spacer()
              }
@@ -55,17 +77,32 @@ public struct ARContainer: UIViewControllerRepresentable {
     /// 현재 발생한 에러. 에러가 없으면 nil
     @Binding var gamePhage: GamePhase
     
+    /// 현재 발생한 에러. 에러가 없으면 nil
+    @Binding var arError: Error?
+    
     /// 현재 인식된 평면 수
     @Binding var currentDetectedPlanes: Int
     
-    public init(gameSettings: GameSettings, currentDetectedPlanes: Binding<Int>) {
+    /// 스캔 시작 트리거
+    @Binding var triggerScanStart: Bool
+    
+    /// 카드 배치 트리거
+    @Binding var triggerPlaceCards: Bool
+    
     public init(
         gameSettings: GameSettings,
         gamePhase: Binding<GamePhase>,
+        arError: Binding<Error?>,
+        currentDetectedPlanes: Binding<Int>,
+        triggerScanStart: Binding<Bool>,
+        triggerPlaceCards: Binding<Bool>
     ) {
         self.gameSettings = gameSettings
         self._gamePhage = gamePhase
+        self._arError = arError
         self._currentDetectedPlanes = currentDetectedPlanes
+        self._triggerScanStart = triggerScanStart
+        self._triggerPlaceCards = triggerPlaceCards
     }
     
     public func makeUIViewController(context: Context) -> ARContainerViewController {
@@ -75,6 +112,30 @@ public struct ARContainer: UIViewControllerRepresentable {
     }
     
     public func updateUIViewController(_ uiViewController: ARContainerViewController, context: Context) {
+        if triggerScanStart {
+            uiViewController.startDetectingPlane()
+            
+            DispatchQueue.main.async {
+                triggerScanStart.toggle()
+            }
+        }
+        
+        if triggerPlaceCards {
+            var raisedError: Error?
+            do {
+                try uiViewController.placeCards()
+            } catch {
+                // 바로 Binding을 통해 Publish하면
+                // `Modifying state during view update, this will cause undefined behavior` 경고 발생
+                // 따라서 메인 큐에서 비동기로 작성 실행
+                raisedError = error
+            }
+            
+            DispatchQueue.main.async {
+                triggerPlaceCards.toggle()
+                arError = raisedError //
+            }
+        }
     }
     
     public func makeCoordinator() -> Coordinator {
@@ -94,6 +155,12 @@ public struct ARContainer: UIViewControllerRepresentable {
         
         public func arContainerDidFindAllPlaneAnchor(_ arContainer: ARContainerViewController) {
             // TODO: 추후에 게임 준비 완료 등 게임 전역 상태를 관리하게 되면, 여기서 "카드 배치 준비 완료"로 전역 상태를 변경
+        }
+        
+        public func didChangeGamePhase(_ arContainer: ARContainerViewController) {
+            DispatchQueue.main.async {
+                self.parent.gamePhage = arContainer.gamePhase
+            }
         }
     }
 }
