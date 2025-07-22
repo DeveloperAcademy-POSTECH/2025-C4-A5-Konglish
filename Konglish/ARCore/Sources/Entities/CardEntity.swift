e49ff10133ef1ebb50b21c6a3de560351788df55
0tak2 <0tak2.code@gmail.com>
2025-07-22 19:24:38 +0900

    ✨ [Feat] CardEntity 구현

---
+++ Konglish/ARCore/Sources/Entities/CardEntity.swift
@@ -0,0 +1,128 @@
+//
+//  CardEntity.swift
+//  ARCore
+//
+//  Created by 임영택 on 7/21/25.
+//
+
+import Foundation
+import RealityKit
+import Combine
+import UIKit
+
+/// 실제 씬에 추가되는 학습 카드의 엔티티
+class CardEntity: Entity, HasModel {
+    // MARK: - Type Properties
+    /// 카드 너비
+    static let cardWidth: Float = 0.255
+    
+    /// 카드 높이
+    static let cardHeight: Float = 0.157
+    
+    /// 카드 두께
+    static let cardDepth: Float = 0.01
+    
+    /// 카드 백그라운드 컬러
+    static let backgroundColor = UIColor(named: "primary01") ?? .red
+    
+    let cardData: GameCard?
+    
+    var displayLabelText: NSAttributedString {
+        let wordEng = NSAttributedString(
+            string: (cardData?.wordEng ?? "N/A") + "\n",
+            attributes: [
+                .foregroundColor: UIColor.black,
+                .font: UIFont.systemFont(ofSize: 64, weight: .bold)
+            ]
+        )
+        let wordKor = NSAttributedString(
+            string: cardData?.wordKor ?? "N/A",
+            attributes: [
+                .foregroundColor: UIColor.black,
+                .font: UIFont.systemFont(ofSize: 48, weight: .medium)
+            ]
+        )
+
+        let combined = NSMutableAttributedString()
+        combined.append(wordEng)
+        combined.append(wordKor)
+        
+        return combined
+    }
+    
+    public init(cardData: GameCard?) {
+        self.cardData = cardData
+        
+        super.init()
+        
+        // 엔티티에 모델 추가
+        self.components[ModelComponent.self] = ModelComponent(
+            mesh: .generateBox(size: [CardEntity.cardWidth, CardEntity.cardDepth, CardEntity.cardHeight]),
+            materials: []
+        )
+        
+        // 텍스쳐 추가
+        let image = imageFrom(
+            text: displayLabelText,
+            font: .systemFont(ofSize: 24),
+            size: CGSize(
+                width: Double(CardEntity.cardWidth) * 1000.0,
+                height: Double(CardEntity.cardHeight) * 1000.0
+            ),
+        )
+        
+        Task { @MainActor in
+            self.model?.materials = [
+                try! await createTextImageMaterial(from: image)
+            ]
+        }
+    }
+    
+    convenience init(cardData: GameCard?, position: SIMD3<Float>) {
+        self.init(cardData: cardData)
+        self.position = position
+    }
+    
+    required convenience init() {
+        self.init(cardData: nil)
+    }
+    
+    /// 특정 텍스트가 포함된 이미지를 생성한다
+    // TODO: 하이파이 디자인에 맞게 카드 이미지, 폰트 등 세팅 필요함
+    func imageFrom(
+        text: NSAttributedString,
+        font: UIFont,
+        size: CGSize,
+        textColor: UIColor = .black,
+        backgroundColor: UIColor = backgroundColor
+    ) -> UIImage {
+        let renderer = UIGraphicsImageRenderer(size: size)
+        let image = renderer.image { context in
+            // 배경 그리기
+            backgroundColor.setFill()
+            context.fill(CGRect(origin: .zero, size: size))
+            
+            // 텍스트 크기 계산
+            let textRect = CGRect(origin: .zero, size: size)
+            let attributedText = text
+            attributedText.draw(in: textRect)
+        }
+        
+        return image
+    }
+    
+    /// UIImage를 CGImage로 변환하고, RealityKit 텍스쳐로 반환한다.
+    func createTextImageMaterial(from image: UIImage) async throws -> Material {
+        guard let cgImage = image.cgImage else {
+            throw NSError(domain: "ImageError", code: 0, userInfo: [NSLocalizedDescriptionKey: "CGImage 변환 실패"])
+        }
+
+        let texture = try await TextureResource.generate(from: cgImage, withName: nil, options: .init(semantic: .normal, compression: .default))
+        // CAUTION: Deprecated되어 `'init(image:withName:options:)'`를 사용하라는 경고가 발생하지만 사용하면 BAD_ACCESS 예외가 발생
+        
+        var material = SimpleMaterial()
+        material.color = .init(texture: MaterialParameters.Texture(texture))
+        
+        return material
+    }
+}
