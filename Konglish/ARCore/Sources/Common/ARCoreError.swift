229b7aaa8d18bcbd12d33cf704bdfa50911e6beb
0tak2 <0tak2.code@gmail.com>
2025-07-24 00:23:02 +0900

    ✨ [Feat] 게임 점수 계산 로직 추가

--- Konglish/ARCore/Sources/Common/ARCoreError.swift
+++ Konglish/ARCore/Sources/Common/ARCoreError.swift
@@ -10,6 +10,7 @@ import Foundation
 /// ARCore 모듈 공통으로 사용되는 에러들
 public enum ARCoreError: Error {
     case insufficientPlanes
+    case cardNotFound
 }
 
 extension ARCoreError: LocalizedError {
@@ -17,6 +18,8 @@ extension ARCoreError: LocalizedError {
         switch self {
         case .insufficientPlanes:
             return "아직 카드를 배치하기에 충분한 평면을 인식하지 못했어요. 공간을 더 촬영해주세요."
+        case .cardNotFound:
+            return "해당 ID로 게임 카드를 찾지 못했습니다."
         }
     }
 }
