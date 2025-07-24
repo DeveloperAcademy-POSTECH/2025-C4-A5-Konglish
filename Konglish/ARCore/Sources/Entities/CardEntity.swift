03c0c2d2d47dd2d1ffe5eb387c2f8fb3e84e072a
0tak2 <0tak2.code@gmail.com>
2025-07-24 21:40:07 +0900

    🔥 [Feat] CardEntity에서 동적 텍스쳐 지정 관련 로직 제거

--- Konglish/ARCore/Sources/Entities/CardEntity.swift
+++ Konglish/ARCore/Sources/Entities/CardEntity.swift
@@ -31,35 +31,13 @@ class CardEntity: Entity, HasModel {
     let cardData: GameCard?
     
     /// 카드 앞면/뒷면 상태 (false: 뒷면, true: 앞면)
-    var isFlipped: Bool = false
+    var isFlipped: Bool = true
     
     /// 카드 발음 성공/실패 상태 (false: 미도전, 실패, true: 성공)
     var isCompleted: Bool = false
     
-    private let logger = Logger.of("CardEntity")
     
-    var displayLabelText: NSAttributedString {
-        let wordEng = NSAttributedString(
-            string: (cardData?.wordEng ?? "N/A") + "\n",
-            attributes: [
-                .foregroundColor: UIColor.black,
-                .font: UIFont.systemFont(ofSize: 64, weight: .bold)
-            ]
-        )
-        let wordKor = NSAttributedString(
-            string: cardData?.wordKor ?? "N/A",
-            attributes: [
-                .foregroundColor: UIColor.black,
-                .font: UIFont.systemFont(ofSize: 48, weight: .medium)
-            ]
-        )
-
-        let combined = NSMutableAttributedString()
-        combined.append(wordEng)
-        combined.append(wordKor)
-        
-        return combined
-    }
+    private let logger = Logger.of("CardEntity")
     
     public init(cardData: GameCard?) {
         self.cardData = cardData
@@ -75,32 +53,16 @@ class CardEntity: Entity, HasModel {
         // 컴포넌트 추가 (검색 목적)
         self.components[CardComponent.self] = CardComponent()
         
-        // 텍스쳐 추가
-        let image = imageFrom(
-            text: displayLabelText,
-            font: .systemFont(ofSize: 24),
-            size: CGSize(
-                width: Double(CardEntity.cardWidth) * 1000.0,
-                height: Double(CardEntity.cardHeight) * 1000.0
-            ),
-        )
+        // 콜리젼
+        self.components[CollisionComponent.self] = CollisionComponent(shapes: [.generateBox(size: [CardEntity.cardWidth, CardEntity.cardDepth, CardEntity.cardHeight])])
         
-        Task {
-            var material: Material
-            do {
-                material = try await createTextImageMaterial(from: image)
-            } catch {
-                let logMessage = "❌ CardEntity: failed to create text image material"
-                    + "=> fallback texture will be applied."
-                    + "error:"
-                logger.error("\(logMessage) \(error)")
-                material = SimpleMaterial(color: CardEntity.backgroundColor, isMetallic: false)
-            }
-            
-            await MainActor.run {
-                self.model?.materials = [material]
-            }
-        }
+        // 동적 텍스쳐 지정을 위한 컴포넌트
+        self.components[DynamicTextureComponent.self] = DynamicTextureComponent(cardData: cardData)
+        
+        // 기본 텍스쳐
+        self.model?.materials = [
+            SimpleMaterial(color: Self.backgroundColor, isMetallic: false)
+        ]
     }
     
     convenience init(cardData: GameCard?, position: SIMD3<Float>) {
@@ -110,44 +72,5 @@ class CardEntity: Entity, HasModel {
     
     required convenience init() {
         self.init(cardData: nil)
-    }
-    
-    /// 특정 텍스트가 포함된 이미지를 생성한다
-    // TODO: 하이파이 디자인에 맞게 카드 이미지, 폰트 등 세팅 필요함
-    func imageFrom(
-        text: NSAttributedString,
-        font: UIFont,
-        size: CGSize,
-        textColor: UIColor = .black,
-        backgroundColor: UIColor = backgroundColor
-    ) -> UIImage {
-        let renderer = UIGraphicsImageRenderer(size: size)
-        let image = renderer.image { context in
-            // 배경 그리기
-            backgroundColor.setFill()
-            context.fill(CGRect(origin: .zero, size: size))
-            
-            // 텍스트 크기 계산
-            let textRect = CGRect(origin: .zero, size: size)
-            let attributedText = text
-            attributedText.draw(in: textRect)
-        }
-        
-        return image
-    }
-    
-    /// UIImage를 CGImage로 변환하고, RealityKit 텍스쳐로 반환한다.
-    func createTextImageMaterial(from image: UIImage) async throws -> Material {
-        guard let cgImage = image.cgImage else {
-            throw NSError(domain: "ImageError", code: 0, userInfo: [NSLocalizedDescriptionKey: "CGImage 변환 실패"])
-        }
-
-        let texture = try await TextureResource.generate(from: cgImage, withName: nil, options: .init(semantic: .normal, compression: .default))
-        // CAUTION: Deprecated되어 `'init(image:withName:options:)'`를 사용하라는 경고가 발생하지만 사용하면 BAD_ACCESS 예외가 발생
-        
-        var material = SimpleMaterial()
-        material.color = .init(texture: MaterialParameters.Texture(texture))
-        
-        return material
     }
 }
