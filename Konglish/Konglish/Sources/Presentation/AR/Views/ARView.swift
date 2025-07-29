05bfe6c5364bbe204a84282db7fa41641fab4d75
0tak2 <0tak2.code@gmail.com>
2025-07-29 21:31:27 +0900

    ✨ [Feat] ARView 구현

---
+++ Konglish/Konglish/Sources/Presentation/AR/Views/ARView.swift
@@ -0,0 +1,100 @@
+//
+//  ARView.swift
+//  Konglish
+//
+//  Created by 임영택 on 7/29/25.
+//
+
+import SwiftUI
+import ARCore
+import SwiftData
+
+struct ARView: View {
+    // MARK: - SwiftData
+    let levelModelID: PersistentIdentifier
+    @Query var levels: [LevelModel]
+    @Query var gameSessions: [GameSessionModel]
+    
+    var selectedLevel: LevelModel? {
+        levels.first { levelModel in
+            levelModel.id == levelModelID
+        }
+    }
+    
+    var selectedGameSession: GameSessionModel? {
+        gameSessions.first { gameSessionModel in
+            gameSessionModel.level.id == levelModelID
+        }
+    }
+    
+    var allPlanesDetected: Bool {
+        arViewModel.currentDetectedPlanes == gameCards.count
+    }
+    
+    // ARCore 데이터 모델
+    var gameCards: [GameCard] {
+        if let selectedGameSession {
+            return selectedGameSession.usedCards.compactMap { GameModelMapper.toGameModel($0.card) }
+        }
+        
+        return []
+    }
+    
+    // MARK: - View Model
+    @State var arViewModel: ARViewModel
+    
+    // MARK: - 게임 세팅을 위한 프로퍼티
+    /// 최소 평면 사이즈
+    let minimumSizeOfPlane: Float = 0.5
+    /// 앞면 이미지 타이틀 폰트
+    let titleFont: UIFont = KonglishFontFamily.NPSFont.extraBold.font(size: 64)
+    /// 앞면 이미지 서브타이틀 폰트
+    let subtitleFont: UIFont = KonglishFontFamily.NPSFont.extraBold.font(size: 32)
+    
+    var body: some View {
+        ARContainer(
+            gameSettings: .init(
+                gameCards: gameCards,
+                minimumSizeOfPlane: minimumSizeOfPlane,
+                fontSetting: .init(
+                    title: titleFont,
+                    subtitle: subtitleFont
+                )
+            ),
+            gamePhase: $arViewModel.gamePhase,
+            arError: $arViewModel.arError,
+            currentDetectedPlanes: $arViewModel.currentDetectedPlanes,
+            currentLifeCounts: $arViewModel.currentLifeCounts,
+            currentGameScore: $arViewModel.currentGameScore,
+            numberOfFinishedCards: $arViewModel.numberOfFinishedCards,
+            flippedCardId: $arViewModel.flippedCardId,
+            triggerScanStart: $arViewModel.triggerScanStart,
+            triggerPlaceCards: $arViewModel.triggerPlaceCards,
+            triggerSubmitAccuracy: $arViewModel.triggerSubmitAccuracy,
+            triggerFlipCard: $arViewModel.triggerFlipCard
+        )
+        .overlay {
+            Group {
+                switch arViewModel.gamePhase {
+                case .initialized:
+                    StartOverlay(arViewModel: arViewModel)
+                case .scanning:
+                    CheckScanOverlay(arViewModel: arViewModel, allPlanesDetected: allPlanesDetected)
+                case .scanned, .playing:
+                    if !arViewModel.showingWordDetailCard {
+                        PlayingGameOverlay(arViewModel: arViewModel)
+                    } else {
+                        OnShowingCardOverlay(arViewModel: arViewModel)
+                    }
+                case .fisished:
+                    if let selectedGameSession {
+                        FinishedOverlay(gameSessionModel: selectedGameSession)
+                    }
+                default:
+                    EmptyView()
+                }
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
