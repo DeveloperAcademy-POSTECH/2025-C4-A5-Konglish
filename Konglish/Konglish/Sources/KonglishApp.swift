c35f526aa5c5beed70919de91b00a187362538b4
JeOng <ikejhc159@gmail.com>
2025-07-30 17:41:36 +0900

    ✨ [Feat] SwiftData 연결
    SwiftData 연결

--- Konglish/Konglish/Sources/KonglishApp.swift
+++ Konglish/Konglish/Sources/KonglishApp.swift
@@ -1,14 +1,49 @@
 import SwiftUI
+import SwiftData
 import Dependency
 
 @main
 struct KonglishApp: App {
-    
     @State var container = DIContainer(navigationRouter: NavigationRouter<AppRoute>())
+
+    private let modelContainer: ModelContainer = {
+        do {
+            let container = try ModelContainer(for: 
+                                                CategoryModel.self,
+                                               CardModel.self,
+                                               LevelModel.self,
+                                               GameSessionModel.self,
+                                               UsedCardModel.self
+            )
+            
+            if isFirstLaunch() {
+                let bootstrapper = DataBootstrapper(context: container.mainContext)
+                bootstrapper.bootstrap()
+            }
+
+            return container
+        } catch {
+            fatalError("SwiftData 컨테이너 초기화 실패: \(error)")
+        }
+    }()
+
     var body: some Scene {
         WindowGroup {
             GuidingView()
                 .environmentObject(container)
+                .modelContainer(modelContainer)
+        }
+    }
+
+    /// 최초 1회 실행 여부 확인
+    private static func isFirstLaunch() -> Bool {
+        let key = "hasBootstrapped"
+        let defaults = UserDefaults.standard
+        if defaults.bool(forKey: key) {
+            return false
+        } else {
+            defaults.set(true, forKey: key)
+            return true
         }
     }
 }
