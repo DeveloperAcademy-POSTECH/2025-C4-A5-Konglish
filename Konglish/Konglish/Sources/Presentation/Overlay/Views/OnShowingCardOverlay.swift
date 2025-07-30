4b95bbfbb2e2fd7a1f1f6d98ce922e00e442ff84
0tak2 <0tak2.code@gmail.com>
2025-07-31 03:06:46 +0900

    💄 [Design] OnShowingCardOverlay 하단 패딩 수정

--- Konglish/Konglish/Sources/Presentation/Overlay/Views/OnShowingCardOverlay.swift
+++ Konglish/Konglish/Sources/Presentation/Overlay/Views/OnShowingCardOverlay.swift
@@ -83,7 +83,7 @@ struct OnShowingCardOverlay: View {
             }
         })
         .safeAreaPadding(.horizontal, UIConstants.topPadding)
-        .safeAreaPadding(.bottom, OnShowingCardConstants.bottomPadding)
+        .safeAreaPadding(.bottom, UIConstants.bottomPadding)
         .safeAreaPadding(.top, UIConstants.topPadding)
         .navigationBarBackButtonHidden(true)
         .onChange(of: detailCardViewModel.heart) { _, newValue in
